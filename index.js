4e56ed07d8ebc102a11fb5dd3304be2867cf2b71
AFG2502 <andresgarcia25022@gmail.com>
2022-03-24 19:31:30 -0500

    aprendimos sobre funciones y constantes

---
+++ index.js
@@ -0,0 +1,69 @@
+/*function getMensaje(){
+    return console.log("hola grupo");
+}
+
+let mensaje = getMensaje();
+console.log(mensaje);
+
+const getNombre2  = () => {
+    console.log("hola grupo");
+}
+n1 = 2;
+n2 = 3;
+
+
+function sumar (){
+    const suma = n1+n2;
+    return suma;
+}
+function division (){
+    const division = n1/n2;
+    return division;
+    if(n2 == 0){
+        return division = "no es posible dividir por 0"
+    }
+}
+function multiplicar (){
+    const multiplicar = n1*n2;
+    return multiplicar;
+}
+function restar(){
+    const restar = n1-n2;
+    return restar;
+}
+
+const resultadoSuma = sumar();
+console.log(resultadoSuma);
+
+const resutadoResta = restar();
+console.log(resutadoResta);
+
+const resultadoMultiplicacion = multiplicar();
+console.log(resultadoMultiplicacion);
+
+const resultadoDivision = division();
+console.log(resultadoDivision);
+
+
+
+
+multiplicar();
+sumar();
+division();
+restar();*/
+
+function suma(numeroUno, numeroDos){
+    const suma = numeroUno + numeroDos;
+    console.log(suma);
+}
+
+const suma2 = (numeroUno, numeroDos) =>{
+    const suma = numeroUno + numeroDos;
+    console.log(suma);
+}
+
+const num1 = 10;
+const num2 = 6;
+
+suma(num1,num2);
+suma2(10, 10)
